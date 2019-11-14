308f4c1b03fde3defa15aff717ec90e5a15b4a01
DenisKruglik <d.kruglik11011@gmail.com>
2019-11-14 12:25:09 +0300

    Created a simple form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'aws-test';
+  text: string;
+
+  isNotificationVisible = false;
+
+  notificationText: string;
+
+  sendMessage() {
+    fetch('https://0jr9lsjft6.execute-api.us-east-2.amazonaws.com/dev/addMessage/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        text: this.text
+      })
+    })
+    .then(() => {
+      this.showNotification('Message sent successfully');
+    })
+    .catch(reason => {
+      console.log(reason);
+      this.showNotification('Something went wrong');
+    });
+  }
+
+  private showNotification(notificationText) {
+    this.notificationText = notificationText;
+    this.isNotificationVisible = true;
+    setTimeout(() => {
+      this.isNotificationVisible = false;
+    }, 3000);
+  }
 }
